5b32f55a07c64a089111a905a8d2f09a4c915abe
Broxxar <danjohnmoran@gmail.com>
2015-04-18 12:25:04 -0400

    YZSorting
    Ties an object's Z position to it's Y position to sort things that are
    visually further back. Mark as static to stop wasted updates for non
    moving entities (eg buildings, props).

---
+++ Assets/Scripts/YZSorting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class YZSorting : MonoBehaviour
+{
+	public bool IsStatic;
+
+	void Start ()
+	{
+		enabled = !IsStatic;
+	}
+
+	void LateUpdate () 
+	{
+		Vector3 newPosition = transform.position;
+		newPosition.z = newPosition.y;
+		transform.position = newPosition;
+	}
+}
