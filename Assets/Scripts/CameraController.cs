0d72f12c60fba238b9a2398e0aec7c7842d7562e
Broxxar <danjohnmoran@gmail.com>
2015-04-18 16:24:19 -0400

    Camera and Player
    Camera and player control, player animations, additional character art
    assets.
    
    Starting on Polaroid printout mechanic.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform FollowTarget;
+	public float MouseInfluence;
+	public float FixedZ;
+	public float FollowStrength;
+	
+	Camera _camera;
+	
+	void Awake ()
+	{
+		_camera = GetComponent<Camera>();	
+		Vector3 newPosition = FollowTarget.position;
+		newPosition.z = FixedZ;
+		transform.position = newPosition;
+	}
+	
+	void LateUpdate ()
+	{
+		Vector3 mousePos = (_camera.ScreenToViewportPoint(Input.mousePosition) * 2);
+		
+		mousePos.x = Mathf.Clamp(mousePos.x-1,-1,1);
+		mousePos.y = Mathf.Clamp(mousePos.y-1,-1,1);
+
+		Vector3 newPosition = Vector3.Lerp(
+			transform.position,
+			FollowTarget.position + mousePos * MouseInfluence,
+			Time.deltaTime * FollowStrength);
+			
+		newPosition.z = FixedZ;
+		transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float MoveSpeed;
+	public float ViewRotationStrength;
+	
+	Animator _anim;
+	Transform _view;
+
+	int _cameraFlashHash = Animator.StringToHash("CameraFlash");
+	int _isWalkingHash = Animator.StringToHash("IsWalking");
+
+	void Awake ()
+	{
+		_anim = GetComponent<Animator>();
+		_view = transform.FindChild("view");
+	}
+	
+	void Update ()
+	{
+		UpdateCameraShoot();
+		UpdateMovement();
+		UpdateView();
+	}
+	
+	void UpdateCameraShoot ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			_anim.SetTrigger(_cameraFlashHash);
+		}
+	}
+	
+	void UpdateMovement ()
+	{
+		Vector3 movement = Vector3.zero;
+		
+		if (Input.GetKey(KeyCode.W))
+			movement.y += 1;
+		if (Input.GetKey(KeyCode.A))
+			movement.x -= 1;
+		if (Input.GetKey(KeyCode.S))
+			movement.y -= 1;
+		if (Input.GetKey(KeyCode.D))
+			movement.x += 1;
+		
+		_anim.SetBool(_isWalkingHash, movement != Vector3.zero);
+		transform.Translate (movement.normalized * MoveSpeed * Time.deltaTime);
+	}
+	
+	void UpdateView ()
+	{
+		Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mouseWorld.z = _view.position.z;
+		Vector3 desiredFacing = (mouseWorld - _view.position).normalized;
+		
+		Quaternion desiredRotation = Quaternion.FromToRotation(Vector3.up, desiredFacing);
+		
+		if (desiredRotation.eulerAngles.y != 0)
+		{
+			Vector3 fixedEuler = desiredRotation.eulerAngles;
+			fixedEuler.y = 0;
+			desiredRotation.eulerAngles = fixedEuler;
+		}
+		
+		_view.rotation = Quaternion.Lerp(_view.rotation, desiredRotation, ViewRotationStrength * Time.deltaTime);
+	}
+}
