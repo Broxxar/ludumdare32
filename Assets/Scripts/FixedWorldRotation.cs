c713a15ceded99ccae07307b9085f00fe96e4059
Broxxar <danjohnmoran@gmail.com>
2015-04-18 14:02:51 -0400

    FoV
    Imported 2DVLS for Field of View stuffs.

---
+++ Assets/Scripts/FixedWorldRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FixedWorldRotation : MonoBehaviour
+{
+	void Update ()
+	{
+		transform.rotation = Quaternion.identity;
+	}
+}
