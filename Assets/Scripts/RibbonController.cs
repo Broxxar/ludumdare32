c0dcd0c9dc6a385fc5319ceea2be35df4c45682c
Mallory Ketcheson <mallory.ketcheson@gmail.com>
2015-04-20 00:28:44 -0400

    RibbonEvent ANIMATION :)

--- Assets/Scripts/RibbonController.cs
+++ Assets/Scripts/RibbonController.cs
@@ -5,6 +5,16 @@ public class RibbonController : MonoBehaviour
 {
 	Animator _anim;
 	ParticleSystem _particleSystem;
-	
-	
+	int _cutHash; 
+
+	void Start(){
+		_particleSystem = transform.GetComponentInChildren<ParticleSystem>();
+		_anim = GetComponent<Animator>();
+		_cutHash = Animator.StringToHash("Cut");
+	}
+
+	public void CutTheRibbon(){
+		_anim.SetTrigger (_cutHash);
+		_particleSystem.Play ();
+	}
 }
